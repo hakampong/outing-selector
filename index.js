6e2a3b6062e46b1bdc836021ae101432f5ca5251
hakampong <hakamkam.01@gmail.com>
2021-07-04 21:16:56 +0800

    Created database
    -created food table but have not fully populate

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function yourName() {
+    var questions = document.getElementById("questions");
     var x = document.getElementById("name").value;
-    document.getElementById("welcome").innerHTML = 'Hello, ' + x;
+    document.getElementById("displayName").innerHTML = 'Hello, ' + x;
+    questions.style.display = "block";
+
+}
+
+function yourFeeling() {
+    var x = document.getElementById("feeling").value;
+    document.getElementById("displayFeeling").innerHTML = 'Hello, ' + x;
 }
