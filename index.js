f62b8d699b2ec829991d701ff883d7f1794ea018
hakampong <hakamkam.01@gmail.com>
2021-07-06 10:10:34 +0800

    Finished questions
    -Finished coding the questions required for the website

--- index.js
+++ index.js
@@ -2,11 +2,54 @@ function yourName() {
     var questions = document.getElementById("questions");
     var x = document.getElementById("name").value;
     document.getElementById("displayName").innerHTML = 'Hello, ' + x;
+    document.getElementById("displayName").style.color = "black";
     questions.style.display = "block";
 
 }
 
 function yourFeeling() {
     var x = document.getElementById("feeling").value;
-    document.getElementById("displayFeeling").innerHTML = 'Hello, ' + x;
+    var hungryQuestions = document.getElementById("hungryQuestions");
+    var sportyQuestions = document.getElementById("sportyQuestions");
+    var lazyQuestions = document.getElementById("lazyQuestions");
+    var adventurousQuestions = document.getElementById("adventurousQuestions");
+    var touristyQuestions = document.getElementById("touristyQuestions");
+    switch(x){
+        case "Hungry":
+            sportyQuestions.style.display = "none";
+            lazyQuestions.style.display = "none";
+            adventurousQuestions.style.display = "none";
+            touristyQuestions.style.display = "none";
+            hungryQuestions.style.display = "block";
+            break;
+        case "Sporty":
+            hungryQuestions.style.display = "none";
+            lazyQuestions.style.display = "none";
+            adventurousQuestions.style.display = "none";
+            touristyQuestions.style.display = "none";
+            sportyQuestions.style.display = "block";
+            break;
+        case "Lazy":
+            hungryQuestions.style.display = "none";
+            sportyQuestions.style.display = "none";
+            adventurousQuestions.style.display = "none";
+            touristyQuestions.style.display = "none";
+            lazyQuestions.style.display = "block";
+        case "Adventurous":
+            hungryQuestions.style.display = "none";
+            sportyQuestions.style.display = "none";
+            adventurousQuestions.style.display = "block";
+            touristyQuestions.style.display = "none";
+            lazyQuestions.style.display = "none";
+        case "Touristy":
+            hungryQuestions.style.display = "none";
+            sportyQuestions.style.display = "none";
+            adventurousQuestions.style.display = "none";
+            touristyQuestions.style.display = "block";
+            lazyQuestions.style.display = "none";
+
+    }
+
+
+
 }
